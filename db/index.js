1f9f6b1b7fc5d2501c46ef6fdba67a8dd4f65538
Jakub <sorok028@umn.edu>
2023-02-16 19:24:28 -0600

    added packages

--- db/index.js
+++ db/index.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose')
-
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/workoutsDatabase'
+require('dotenv').config() // Add this line
 
 mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-        console.log('Successfully connected to MongoDB.')
-    })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
-
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+app.use(express.static(`${__dirname}/client/build`))
 
 // app.use() middleware here ^ ///////////////////
 
@@ -21,4 +22,8 @@ app.use('/api', routes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+   })
+   
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
